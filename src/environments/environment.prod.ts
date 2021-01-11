f7efbfc8ce0ebbbe74a669427e6be51b0af81547
ivelin-web <72993041+ivelin-web@users.noreply.github.com>
2021-01-11 17:48:20 +0200

    Firebase added to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+      firebase: {
+        apiKey: 'AIzaSyByAX7bOkj9wEDxgXn2Bu3YR7cKy2Qvs3k',
+        authDomain: 'ng-forum-21.firebaseapp.com',
+        projectId: 'ng-forum-21',
+        storageBucket: 'ng-forum-21.appspot.com',
+        messagingSenderId: '245062704260',
+        appId: '1:245062704260:web:94928d05e99e37f7d076d1',
+        measurementId: 'G-Q54TTJ4YV8',
+    },
 };
